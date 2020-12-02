7491746afeb8038a56e34686657d2388a573c69e
Your Name <you@example.com>
2020-12-02 21:03:39 +0545

    yes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+//gh-pages
 // React imports
 import React from 'react'
 import './App.css';
@@ -7,7 +8,7 @@ import {BrowserRouter as Router, HashRouter, Route} from 'react-router-dom'
 import { Link } from 'react-router-dom';
 
 // semantic-UI imports
-import {, Label, Grid, Advertisement, Container, Segment, Icon, Modal } from 'semantic-ui-react';
+import {Label, Grid, Advertisement, Container, Segment, Icon, Modal } from 'semantic-ui-react';
 
 // components imports here
 import Header from "./components/Header"
